e3791b9c5624fd7c2f9d304f27f09d051fc57537
Enrico Lovisotto <enricolovisotto@gmail.com>
2016-09-25 14:33:49 +0200

    Completed authentication process
    App secret key is now moved to global variable, fixing security hole

--- spese_app/controller.py
+++ spese_app/controller.py
@@ -1,14 +1,15 @@
 from functools import wraps
 from datetime import datetime, date
+import os
 
 from flask import *
 from flask_login import LoginManager, login_user, current_user, logout_user
 
 from spese_app.model import *
-from spese_app.forms import LoginForm
+from spese_app.forms import *
 
 app = Flask(__name__)
-app.secret_key = 'asdasd8atsd7r6872y12be019asdjho2182d9b'
+app.secret_key = os.environ['SECRET_KEY']
 
 # login manager initialization
 login_manager = LoginManager()
@@ -21,15 +22,23 @@ def load_user(user_id):
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	form = LoginForm(request.form)
-
+	
 	if form.validate_on_submit() and request.method=='POST':
-		user = session.query(User).filter(
-			User.username==form.username.data,
-			User.password==form.password.data
-		).first()
-		if user!=None:
-			login_user(user, remember=False)
+		user_to_log = authenticate_user(
+			apartment = form.apartment.data,
+			username = form.username.data,
+			password = form.password.data
+		)
+
+		if user_to_log:
+			login_user(user_to_log, remember=False)
 			# return redirect(url_for("main_page"))
 		else:
 			flash("Password o username errati")
 	return render_template("login.html", form=form)
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+	if current_user.is_authenticated:
+		logout_user()
+	return redirect(url_for("login"))
--- spese_app/forms.py
+++ spese_app/forms.py
@@ -2,7 +2,11 @@ from wtforms import StringField, PasswordField, SubmitField
 from flask_wtf import Form
 from wtforms.validators import DataRequired, NumberRange, Length, InputRequired
 
+__all__ = ['LoginForm']
+
+
 class LoginForm(Form):
-	username = StringField(render_kw={'placeholder': 'Username'})
-	password = PasswordField(render_kw={'placeholder': 'Password'})
+	apartment = StringField('Appartamento')
+	username = StringField('Nome utente')
+	password = PasswordField('Password')
 	login_button = SubmitField('Login')
